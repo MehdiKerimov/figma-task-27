704e1fc9ff1bf18ace86d4af60771ed45d448482
MehdiKerimov <mehdikerimovv208@gmail.com>
2024-02-17 15:38:56 +0400

    Figma task 27

--- pages/loginPage/index.js
+++ pages/loginPage/index.js
@@ -1,25 +1,17 @@
 let openBtn=document.querySelector('.open-btn')
 let closeBtn=document.querySelector('.close-btn')
-let input =document.querySelector('.password-input')
+let input =document.querySelector('.email-input')
 
 openBtn.addEventListener('click',hidePassword)
 closeBtn.addEventListener('click',showPassword)
 
 function showPassword(){
-    console.log('dkjddf')
     input.type='text'
     openBtn.style.display='block'
     closeBtn.style.display='none'
 }
 function hidePassword(){
-input.type='password'
+input.type='email'
 closeBtn.style.display='block'
 openBtn.style.display='none'
 }
-let array=[1,2,3,4]
-let json=JSON.stringify(array)
-localStorage.setItem('numbers',json)
-
-let user=localStorage.getItem('user')
-let json2=JSON.parse(user)
-console.log(json2.name)
