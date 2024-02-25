004364993ab715f5d04ee27323f010b38c842358
MehdiKerimov <mehdikerimovv208@gmail.com>
2024-02-25 16:25:48 +0400

    Figma task 27

--- pages/registerPage/index.js
+++ pages/registerPage/index.js
@@ -2,6 +2,11 @@ let openBtn=document.querySelector('.open-btn')
 let closeBtn=document.querySelector('.close-btn')
 let passwordInput =document.querySelector('.password-input')
 let form=document.querySelector('form')
+let emailInput=document.querySelector('.emailInput')
+let errorMsg=document.querySelector('.Error-msg')
+let existError=document.querySelector('.exist-error')
+let userExist=false
+
 
 openBtn.addEventListener('click',hidePassword)
 closeBtn.addEventListener('click',showPassword)
@@ -17,18 +22,68 @@ passwordInput.type='password'
 closeBtn.style.display='block'
 openBtn.style.display='none'
 }
-// let array=[1,2,3,4]
-// let json=JSON.stringify(array)
-// localStorage.setItem('numbers',json)
 
-// let user=localStorage.getItem('user')
-// let json2=JSON.parse(user)
-// console.log(json2.name)
+
+// Database yaradilmasi
+
+function createDatabase(){
+    let=array=[]
+    localStorage.setItem('users',JSON.stringify(array))
+}
+
+// createDatabase()
+
 
 // submit action
-form.addEventListener('click',handleRegister)
+form.addEventListener('submit',handleRegister)
 function handleRegister(e){
     e.preventDefault()
-    console.log(passwordInput.value)
-    
+if(emailInput.value && passwordInput.value){
+checkUser()
+errorMsg.style.display='none'
 }
+else{
+errorMsg.style.display='block'
+}
+  
+}
+
+// istifadecinin yoxlanmasi
+function checkUser(){
+let users=localStorage.getItem('users')
+let usersData=JSON.parse(users)
+for (let index = 0; index < usersData.length; index++) {
+if(usersData[index].email==emailInput.value){
+    userExist=true
+    break
+}
+else{
+    userExist=false
+}
+
+}
+if(userExist){
+    existError.style.display='block'
+}
+else{
+    existError.style.display='none'
+    createUser()
+}
+
+}
+
+
+// istifadecinin yaradilmasi
+function createUser(){
+    let user={
+        email:emailInput.value,
+        password:passwordInput.value
+    
+    }
+let usersData=localStorage.getItem('users')
+let user5=JSON.parse(usersData)
+user5.push(user)
+localStorage.setItem('users',JSON.stringify(user5))
+form.reset()
+}
+
